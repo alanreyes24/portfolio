d714b8ac87ba9de47cc5cd78553849be4e1c04a7
Alan Reyes <alanreyes6747@gmail.com>
2024-11-17 12:02:28 -0600

    Allow scroll on right side of page only

--- src/App.js
+++ src/App.js
@@ -1,41 +1,87 @@
-import "./App.css";
+import React from "react";
 import AboutMe from "./components/aboutme/aboutme.js";
 
 function App() {
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          fontFamily: "Arial, sans-serif",
-          marginTop: "50px",
-          marginBottom: "50px",
-          marginLeft: "100px",
-          marginRight: "100px",
-        }}
-      >
+    <div className="flex justify-center items-center h-screen font-sans mt-12 mb-12 mx-24">
+      <div className="bg-red-100 h-screen">
         <AboutMe />
-        <div
-          style={{
-            overflow: "scroll",
-            width: "50%",
-            height: "100%",
-            backgroundColor: "aqua",
-            // border: 1px black solid,
-            // padding: 2%,
-            // width: 300px,
-            // margin: 0 auto,
-            // white-space: nowrap,
-            // font-size: large,
-          }}
-        >
-          Scroll
-        </div>
       </div>
-    </>
+      <div className="w-[50%] bg-blue-100 max-h-screen sticky top-0 overflow-y-auto">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed
+        lectus fermentum dui vulputate vehicula ac quis dolor. Sed sed dolor
+        elit. Suspendisse at tincidunt urna. Suspendisse scelerisque eget ipsum
+        quis placerat. Vivamus in rutrum enim. Fusce in enim neque. Etiam
+        pharetra sodales orci, sed feugiat tellus rhoncus non. Suspendisse
+        bibendum lectus rhoncus, hendrerit neque sed, efficitur erat. Etiam
+        euismod lacus et magna suscipit condimentum. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Ut placerat lacinia tellus, vel porttitor
+        tellus suscipit ac. Nullam et fringilla felis. Vestibulum venenatis at
+        nunc nec tincidunt. Etiam ac neque libero. Etiam vel finibus turpis, non
+        porta arcu. Mauris quis ante tellus. Pellentesque lacinia urna sit amet
+        felis auctor, sed aliquam enim egestas. Sed non lacus at nunc
+        consectetur fringilla. Ut ac posuere arcu, at gravida augue. Nulla ut
+        auctor ante, et venenatis tortor. Sed quis metus sit amet ex consequat
+        tempor in sollicitudin massa. Nullam ut faucibus nunc, quis viverra
+        quam. Curabitur quis commodo metus, sed consectetur augue. Vivamus
+        euismod placerat massa, quis posuere felis efficitur vel. Aenean ex
+        quam, sagittis nec neque in, porta blandit risus. Maecenas iaculis augue
+        sit amet ante sodales pulvinar. Aliquam in feugiat risus. Donec tempus
+        id lectus in pharetra. Sed congue convallis porta. Aliquam tincidunt non
+        nibh eget suscipit. Donec laoreet dictum enim vel sollicitudin. Nam in
+        ex facilisis, commodo neque eget, egestas ligula. Ut ultricies dolor in
+        blandit imperdiet. Curabitur in augue quis nisi porttitor fermentum vel
+        sit amet lacus. Aliquam aliquet, leo ac egestas semper, libero nisl
+        interdum mauris, ac mattis ipsum nulla ac elit. Pellentesque vel diam et
+        nulla vestibulum lacinia. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. In at bibendum nunc, eget
+        feugiat lorem. Suspendisse imperdiet vulputate sapien, rutrum fermentum
+        nunc dapibus sit amet. Donec bibendum pellentesque mi sit amet faucibus.
+        Sed vel consectetur nunc. Aliquam posuere magna ac sapien consequat,
+        semper commodo metus maximus. Fusce condimentum facilisis tristique.
+        Donec sed tempor sem, at gravida metus. In hac habitasse platea
+        dictumst. Sed orci urna, sagittis ut lacus vitae, semper rutrum libero.
+        Suspendisse mattis ultrices est in pellentesque. Suspendisse malesuada,
+        nunc eu blandit commodo, eros nisl condimentum tortor, quis aliquet orci
+        risus nec lacus. Aenean vitae leo enim. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Proin sed lectus fermentum dui vulputate
+        vehicula ac quis dolor. Sed sed dolor elit. Suspendisse at tincidunt
+        urna. Suspendisse scelerisque eget ipsum quis placerat. Vivamus in
+        rutrum enim. Fusce in enim neque. Etiam pharetra sodales orci, sed
+        feugiat tellus rhoncus non. Suspendisse bibendum lectus rhoncus,
+        hendrerit neque sed, efficitur erat. Etiam euismod lacus et magna
+        suscipit condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Ut placerat lacinia tellus, vel porttitor tellus suscipit ac.
+        Nullam et fringilla felis. Vestibulum venenatis at nunc nec tincidunt.
+        Etiam ac neque libero. Etiam vel finibus turpis, non porta arcu. Mauris
+        quis ante tellus. Pellentesque lacinia urna sit amet felis auctor, sed
+        aliquam enim egestas. Sed non lacus at nunc consectetur fringilla. Ut ac
+        posuere arcu, at gravida augue. Nulla ut auctor ante, et venenatis
+        tortor. Sed quis metus sit amet ex consequat tempor in sollicitudin
+        massa. Nullam ut faucibus nunc, quis viverra quam. Curabitur quis
+        commodo metus, sed consectetur augue. Vivamus euismod placerat massa,
+        quis posuere felis efficitur vel. Aenean ex quam, sagittis nec neque in,
+        porta blandit risus. Maecenas iaculis augue sit amet ante sodales
+        pulvinar. Aliquam in feugiat risus. Donec tempus id lectus in pharetra.
+        Sed congue convallis porta. Aliquam tincidunt non nibh eget suscipit.
+        Donec laoreet dictum enim vel sollicitudin. Nam in ex facilisis, commodo
+        neque eget, egestas ligula. Ut ultricies dolor in blandit imperdiet.
+        Curabitur in augue quis nisi porttitor fermentum vel sit amet lacus.
+        Aliquam aliquet, leo ac egestas semper, libero nisl interdum mauris, ac
+        mattis ipsum nulla ac elit. Pellentesque vel diam et nulla vestibulum
+        lacinia. Orci varius natoque penatibus et magnis dis parturient montes,
+        nascetur ridiculus mus. In at bibendum nunc, eget feugiat lorem.
+        Suspendisse imperdiet vulputate sapien, rutrum fermentum nunc dapibus
+        sit amet. Donec bibendum pellentesque mi sit amet faucibus. Sed vel
+        consectetur nunc. Aliquam posuere magna ac sapien consequat, semper
+        commodo metus maximus. Fusce condimentum facilisis tristique. Donec sed
+        tempor sem, at gravida metus. In hac habitasse platea dictumst. Sed orci
+        urna, sagittis ut lacus vitae, semper rutrum libero. Suspendisse mattis
+        ultrices est in pellentesque. Suspendisse malesuada, nunc eu blandit
+        commodo, eros nisl condimentum tortor, quis aliquet orci risus nec
+        lacus. Aenean vitae leo enim.
+      </div>
+    </div>
   );
 }
 
