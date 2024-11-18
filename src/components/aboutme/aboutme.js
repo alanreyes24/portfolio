aa0da69598ec5805137d1270bbe69268458c8b1c
Alan Reyes <alanreyes6747@gmail.com>
2024-11-17 20:33:48 -0600

    Add gradient background

--- src/components/aboutme/aboutme.js
+++ src/components/aboutme/aboutme.js
@@ -4,9 +4,16 @@ function AboutMe() {
   return (
     <>
       <div>
-        <div>Alan Reyes</div>
-        <div> First Year Student @ Washington University in St. Louis </div>
-        <div>
+        <h1 class="m-2 text-4xl font-bold tracking-tight sm:text-5xl text-white">
+          <a href="/" class="hover:underline">
+            Alan Reyes
+          </a>
+        </h1>
+        <div class="m-2 text-1xl font-bold tracking-tight text-white">
+          Business & Computer Science Student @ Washington University in St.
+          Louis
+        </div>
+        <div class="m-2 tracking-tight text-white">
           I create data-driven insights and develop seamless digital solutions
           to solve real-world problems.
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, TSX files in the src directory
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orangeRed: "#FF4545",
+        muteOrange: "#FBD288",
+        gradientYellow: "#FCF596",
+      },
+    },
   },
   plugins: [],
 };
