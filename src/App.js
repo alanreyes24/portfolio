ba8477228f88b6d9d414282bcbca3f66919b9f0f
Alan Reyes <alanreyes6747@gmail.com>
2024-11-20 23:08:18 -0600

    Add VirtualStudyGroup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import AboutMe from "./components/aboutme/aboutme.js";
 import Description from "./components/description/description.js";
-import Card from "./components/card/card.js";
 import Navigator from "./components/navigator/navigator.js";
 
 function App() {
   return (
-    <div class="bg-gradient-to-r from-blue-500 to-blue-500">
+    <div class="bg-gradient-to-r from-sky-500 to-teal-500">
       <div class="flex justify-center items-center h-screen font-sans mt-12 mb-12 mx-24">
         <div class="h-screen">
           <Description />
@@ -14,9 +13,39 @@ function App() {
         </div>
         <div class="w-[70%] max-h-screen sticky top-0 overflow-y-auto">
           <AboutMe />
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
+          <div id="bettertyping">
+            <div class="m-2 text-white text-1xl font-bold">
+              bettertyping - a sleek, minimalist typing test website{" "}
+            </div>
+            <div class="text-white m-2">
+              {" "}
+              bettertyping is a simple and effective typing platform designed to
+              help users practice and improve their skills. It offers a variety
+              of test modes, including timed challenges, to suit different
+              preferences and goals. Users can see how they stack up against
+              others through a competitive leaderboard, fostering motivation and
+              engagement. The platform is optimized for smooth performance
+              across devices, featuring a clean, user-friendly interface. Built
+              with React, Node.js, and MongoDB, and hosted on Railway,
+              BetterTyping provides a reliable and accessible way for anyone to
+              enhance their typing abilities.
+            </div>
+          </div>
+          <div id="virtualstudygroup">
+            <div class="m-2 text-white 1xl font-bold">
+              {" "}
+              Virtual Study Group - a MacOS group studying app{" "}
+            </div>
+            <div class="text-white m-2">
+              {" "}
+              Designed for collaborative learning, Virtual Study Group is a
+              macOS app that connects students in virtual rooms where they can
+              study together. Developed with Swift and SwiftUI and supported by
+              Firebase, the app utilizes WebRTC to provide tools like screen
+              sharing, video conferencing, and a shared Pomodoro timer to
+              enhance focus and productivity.
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/aboutme/aboutme.js
+++ src/components/aboutme/aboutme.js
@@ -5,33 +5,34 @@ function AboutMe() {
     <>
       <div class="text-white m-5">
         I’m a developer passionate about creating sleek, intuitive, and
-        efficient digital experiences. My focus lies at the intersection of
-        design and technology, where I strive to build applications that are not
-        only visually appealing but also highly functional and
-        performance-driven.
+        efficient digital experiences that seamlessly blend design and
+        technology. My favorite work lies at the intersection of design and
+        development, building applications that are not only visually stunning
+        but also highly functional and performance-driven.
       </div>
       <div class="text-white m-5">
-        Currently, I’m a freshman at Washington University in St. Louis,
-        pursuing a degree in Business and Computer Science. I’ve developed
-        projects like **BetterTyping**, a minimalist typing test website that
-        combines clean design, robust backend engineering, and advanced data
-        visualizations to enhance user engagement. This project highlights my
-        interest in building tools that merge utility with user-centric design.
+        Currently, I'm a freshman at Washington University in St. Louis,
+        pursuing a dual degree in Business and Computer Science. I’ve developed
+        projects like <strong>BetterTyping</strong>, a minimalist typing test
+        website that combines clean design, robust backend engineering, and
+        advanced data visualizations to enhance user engagement. This project
+        highlights my interest in building tools that merge utility with
+        user-centric design.
       </div>
       <div class="text-white m-5">
-        {" "}
-        I’ve also worked on diverse personal and collaborative initiatives, from
-        web applications with React and Node.js to developing macOS apps
-        leveraging Swift and SwiftUI. These experiences have deepened my
-        understanding of creating scalable, user-friendly solutions and
-        sharpened my problem-solving skills.
+        In the past, I've worked on a diverse range of personal and
+        collaborative initiatives—from web applications using React and Node.js
+        to macOS apps developed with Swift and SwiftUI. These experiences have
+        deepened my understanding of creating scalable, user-friendly solutions
+        and sharpened my problem-solving skills.
       </div>
       <div class="text-white m-5">
-        When I’m not coding, I enjoy exploring new technologies, working on
-        creative side projects, and finding inspiration through gaming, design,
-        and everyday interactions. Whether it’s building something entirely new
-        or improving existing systems, I’m excited by the opportunity to bring
-        ideas to life through development.
+        In my spare time, I enjoy walking through local parks, diving into a
+        good book, and watching soccer matches. I love working on creative side
+        projects and find inspiration through gaming, design, and everyday
+        interactions. Whether it’s exploring a new hobby or engaging in
+        activities I love, I’m always excited to discover and create new
+        experiences.
       </div>
     </>
   );
